2441f05f9870fd96a0392be2292ce196ea8d832f
Mohamed Haridy <mohamed.haridy@avy.health>
2020-10-16 11:49:58 +0200

    Add templates and route.

--- uebung2/uebung2_Code/helloworld/urls.py
+++ uebung2/uebung2_Code/helloworld/urls.py
@@ -1,4 +1,4 @@
-"""uebung2_Code URL Configuration
+"""helloworld URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', TemplateView.as_view(template_name="home.html"), name="home")
 ]
