8d13a13cf2da866352cae57426f1635543dc2d42
Mohamed Haridy <mohamed.haridy@avy.health>
2020-10-16 11:21:24 +0200

    Init. uebung2 project

---
+++ uebung2/uebung2_Code/uebung2_Code/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for uebung2_Code project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uebung2_Code.settings')
+
+application = get_wsgi_application()
