ce1c751393fa490afd98834b97e2a8d022245329
mohamed haridy <m.haridy@ideazcap.com>
2021-01-24 16:33:59 +0100

    Add new game using function view

--- uebung5/Uebung5_PyCharm_project_cloned-u4/ComputerGame/urls.py
+++ uebung5/Uebung5_PyCharm_project_cloned-u4/ComputerGame/urls.py
@@ -4,8 +4,7 @@ from . import views
 urlpatterns = [
     path('show/', views.computer_games_list, name='games-list'),
     path('show/<int:pk>/', views.computer_game_details, name='game-detail'),
-    path('add/', views.ComputergameCreateView.as_view(), name='game-create'),
+    path('add/', views.create_computer_game, name='game-create'),
     path('<pk>/delete/', views.ComputergameDeleteView.as_view(), name='game-confirm-delete'),
-
 
 ]
--- uebung5/Uebung5_PyCharm_project_cloned-u4/ComputerGame/views.py
+++ uebung5/Uebung5_PyCharm_project_cloned-u4/ComputerGame/views.py
@@ -4,6 +4,7 @@ from django.views.generic import CreateView, DetailView, ListView, DeleteView
 from .models import ComputerGame
 from .forms import GameForm
 
+
 def computer_games_list(request):
     all_games = ComputerGame.objects.all()
     context = {'all_games': all_games}
@@ -18,15 +19,28 @@ def computer_game_details(request, **kwargs):
     return render(request, 'game-detail.html', context)
 
 
-class ComputergameCreateView(CreateView):
-    model = ComputerGame
-    form_class = GameForm
-    template_name = 'game-create.html'  # Default: book_form.html
-    success_url = reverse_lazy('games-list')
+def create_computer_game(request):
+    if request.method == 'POST':
+        form_in_my_function_based_view = GameForm(request.POST)
+        form_in_my_function_based_view.instance.user = request.user
+        if form_in_my_function_based_view.is_valid():
+            form_in_my_function_based_view.save()
+        else:
+            pass
+            print(form_in_my_function_based_view.errors)
 
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super().form_valid(form)
+        return redirect('games-list')
+
+    else:  # request.method == 'GET'
+        form_in_my_function_based_view = GameForm()
+        print(form_in_my_function_based_view)
+        context = {'form': form_in_my_function_based_view}
+        return render(request, 'game-create.html', context)
+
+
+def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
 
 
 class ComputergameDeleteView(DeleteView):
