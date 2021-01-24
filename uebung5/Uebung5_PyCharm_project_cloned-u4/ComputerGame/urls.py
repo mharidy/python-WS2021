a52f25a237bf337789a0a95fd4f2da30d6c629ac
mohamed haridy <m.haridy@ideazcap.com>
2021-01-24 14:00:24 +0100

    Use function view- list & details

--- uebung5/Uebung5_PyCharm_project_cloned-u4/ComputerGame/urls.py
+++ uebung5/Uebung5_PyCharm_project_cloned-u4/ComputerGame/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('show/', views.ComputerGameListView.as_view(), name='games-list'),
-    path('show/<int:pk>/', views.ComputergameDetailView.as_view(), name='game-detail'),
+    path('show/', views.computer_games_list, name='games-list'),
+    path('show/<int:pk>/', views.computer_game_details, name='game-detail'),
     path('add/', views.ComputergameCreateView.as_view(), name='game-create'),
     path('<pk>/delete/', views.ComputergameDeleteView.as_view(), name='game-confirm-delete'),
 
--- uebung5/Uebung5_PyCharm_project_cloned-u4/ComputerGame/views.py
+++ uebung5/Uebung5_PyCharm_project_cloned-u4/ComputerGame/views.py
@@ -1,20 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView, DeleteView
 from .models import ComputerGame
 from .forms import GameForm
 
+def computer_games_list(request):
+    all_games = ComputerGame.objects.all()
+    context = {'all_games': all_games}
+    return render(request, 'game-list.html', context)
 
-class ComputerGameListView(ListView):
-    model = ComputerGame
-    context_object_name = 'all_games'  # Default: object_list
-    template_name = 'game-list.html'  # Default: book_list.html
 
-
-class ComputergameDetailView(DetailView):
-    model = ComputerGame
-    context_object_name = 'that_game'  # Default: book
-    template_name = 'game-detail.html'  # Default: book_detail.html
+def computer_game_details(request, **kwargs):
+    print(kwargs)
+    computer_game_id = kwargs['pk']
+    selected_game = ComputerGame.objects.get(id=computer_game_id)
+    context = {'that_game': selected_game}
+    return render(request, 'game-detail.html', context)
 
 
 class ComputergameCreateView(CreateView):
