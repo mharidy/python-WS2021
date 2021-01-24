f0682a87e4441eb3742b485ede50658e785c828a
mohamed haridy <m.haridy@ideazcap.com>
2021-01-24 13:27:10 +0100

    Update ue4 -> ue5 and replace ue4 with ue5 - ue4 works

--- uebung5/Uebung5_PyCharm_project_cloned-u4/uebung5/urls.py
+++ uebung5/Uebung5_PyCharm_project_cloned-u4/uebung5/urls.py
@@ -1,4 +1,4 @@
-"""uebung4 URL Configuration
+"""uebung5 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
